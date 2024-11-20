5c300eb729b5ca58d9bbf7804fefae82bc2cd1ac
Mason ma <MAMA14@oocl.com>
2024-11-20 14:53:12 +0800

    refactor: rename "\\s+" as magic string

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,13 +1,16 @@
 import java.util.*;
 
 public class WordFrequencyGame {
+
+    public static final String Space = "\\s+";
+
     public String getWordFrequency(String sentence) {
-        if (sentence.split("\\s+").length == 1) {
+        if (sentence.split(Space).length == 1) {
             return sentence + " 1";
         } else {
             try {
                 //split the input string with 1 to n pieces of spaces
-                String[] words = sentence.split("\\s+");
+                String[] words = sentence.split(Space);
 
                 List<WordFrequency> frequencies = new ArrayList<>();
                 for (String word : words) {
